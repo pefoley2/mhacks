85038c3e7358f2bbc08a35a06f8fbe0008205f2a
David Steinberg <dmstein@terpmail.umd.edu>
2014-01-17 23:48:51 -0500

    Use loop instead of controller

--- driver.js
+++ driver.js
@@ -1,17 +1,4 @@
- var my_controller = new Leap.Controller({enableGestures: true});
-
- my_controller.on('frame', function(frame_instance){
-
- 	$("#fingerCount").text(frame_instance.fingers.length);
- 	$("#handCount").text(frame_instance.hands.length);
-
- });
-
- my_controller.on('connect', function(){
-    setInterval(function(){
-      var frame = my_controller.frame();
-    }, 500);
-    alert("Connected");
-  });
-
-  my_controller.connect();
+Leap.loop(function(obj) {
+	$("#fingerCount").text(obj.fingers.length);
+	$("#handCount").text(obj.hands.length);
+});
